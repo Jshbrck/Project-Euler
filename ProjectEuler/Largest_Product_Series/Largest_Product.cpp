65669c76505ed4db13ba7c5873c85ac42d2463fe
Jshbrck <36238283+Jshbrck@users.noreply.github.com>
2018-02-10 21:31:54 -0600

    Update Largest_Product.cpp

--- ProjectEuler/Largest_Product_Series/Largest_Product.cpp
+++ ProjectEuler/Largest_Product_Series/Largest_Product.cpp
@@ -1,15 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/* 
- * File:   main.cpp
- * Author: jshbr
- *
- * Created on December 11, 2017, 6:40 PM
- */
 #include <iostream>
 #include <string>
 #include <algorithm>
